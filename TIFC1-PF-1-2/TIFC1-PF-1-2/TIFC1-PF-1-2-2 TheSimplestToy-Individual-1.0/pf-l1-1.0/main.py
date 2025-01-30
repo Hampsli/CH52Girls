e440aa0bad00eae2ca54e7254809dc31e203d2f8
AzulSR <139704806+AzulSR@users.noreply.github.com>
2025-01-29 22:30:12 -0600

    Hice el código de simplesToy o madLib
    Se solicitan palabras al usuario/a para incorporarlas en un texto e imprir una historia. Para esto se usaron las funciones input y print, además de cadenas de caracteres.

---
+++ TIFC1-PF-1-2/TIFC1-PF-1-2/TIFC1-PF-1-2-2 TheSimplestToy-Individual-1.0/pf-l1-1.0/main.py
@@ -0,0 +1,19 @@
+# print("What is your first name?")
+# first_name = input()
+# print("What is your last name?")
+# last_name = input()
+# print("Hey " + first_name + " " + last_name + "!")
+
+# print('They feel overwhelmed but are determined to complete the course.')
+
+#Mad Lib
+
+name= input ("let's play a game, write a name ")
+job_name= input("write a job name ")
+first_adjective = input("write an adjetive ")
+second_adjective = input("write a second adjetive ")
+first_food = input("write a food ")
+second_food = input("write a second food ")
+feeling = input("finally write a feeling ")
+
+print(f"{name} started their first Generation course today. They are training as a {job_name}. They found their cohort to be very {first_adjective} but their teacher was, at least, {second_adjective}. For lunch they have {first_food} and {second_food} while reviewing their notes. They feel {feeling} but are determined to complete the course.")
