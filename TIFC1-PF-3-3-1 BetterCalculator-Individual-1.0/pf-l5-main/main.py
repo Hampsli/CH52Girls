dfc345bf0c21835a04cbe6f015c7c5ce441118a0
EdnaEustaquio <eustaquiosihsa@gmail.com>
2025-01-29 22:28:22 -0600

    cambie funciones suma

--- TIFC1-PF-3-3-1 BetterCalculator-Individual-1.0/pf-l5-main/main.py
+++ TIFC1-PF-3-3-1 BetterCalculator-Individual-1.0/pf-l5-main/main.py
@@ -1,6 +1,22 @@
-def main():
-  print("Hello learners!")
-#Mensajito de prueba
 
-if __name__=="__main__":
-  main()
+print("Hello learners!")
+def SumaLista(): 
+    Lista=[]
+  
+    for i in range(3):
+        Lista.append(input("porfavor, dame un numero: "))
+    print("la lista es:",Lista[0])
+
+Num=int(input("Si quieres hacer 1)Sumar una lista,  coloca el numero"))
+if  Num==1:
+    SumaLista()
+elif Num == 2:
+    print("Haz b")
+elif Num == 3:
+    print("Haz c")
+else:
+    print("Haz d")
+
+
+
+    
