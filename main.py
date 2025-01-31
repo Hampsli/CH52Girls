5ff213a7ed9d608ac61d8132d5bdee21a087233e
M0-00n <m00np1n5678@gmail.com>
2025-01-30 20:25:58 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,25 @@
-def main():
-  print("Hello learners!")
+#Código que pide un nuemro y da dato divertidos sobre ese numeros
 
-#Función para ingresar los numeros a la lista
-def intronum(listnum):
-  print("introduzca la cantidad de numeros a operar")
-  cantNum=int(input())
-  listnum =[] 
-  #Código para pedir los numeros
-  for numero in range(cantNum):
-    print("ingrese el numero: ")
-    numero=float(input())
-    listnum.append(numero)
-  return listnum
+#Se importan los diccionarios necesarios
+import json
+import requests
 
-#Funcion para sumar los numeros
-def addmultiplenumbers(listnum):
-  suma=0
-  for numero in listnum:
-      suma=suma+numero
-  return suma
+def trivia_fetch(number):
+    
+    #El url se debe modificar de acuerdo al numero ingresado, se guarda en una variable
+    #{} es un marcador de posición y permite insertar en la cadena el valor de number
+    url="http://numbersapi.com/{}?json".format(number) 
 
+    #Código sesión
+    response=requests.get(url)
+    trivia=json.loads(response.content)
+    return trivia
+    
+#Se solicita el numero 
+print("Ingrese el numero")
+number=input()
+#Se llama a la función
+print(trivia_fetch(number))
 
-#Funcion para multiplicar los numeros
-def multiplymultiplenumbers(listnum):
-    multi=1
-    for numero in listnum:
-        multi=multi*numero
-        
-    return multi
-
-#Funcion para definir si el numero es par o impar
-def isiteven(num):
-  if int(num) % 2 == 0:
-   return True 
-  else:
-   return False
 
   
-#Funcion para definir si el numero entero
-def isitaninteger(num): #Se utiliza funcion de python erifica si la variable num es del tipo entero int
-  if isinstance(num, int):
-        return True
-  elif isinstance(num, float) and num.is_integer():
-        return True
-  else:
-        return False
-    
-if __name__=="__main__":
-  main()
-  # Resultado de la suma de los números
-  listnum = intronum()
-  addmultiplenumbers(listnum)
-  listnum.clear()
-
-  #Resultado de la multiplicación de los numeroa
-  intronum(listnum)
-  multiplymultiplenumbers(listnum)
-  listnum.clear()
-  #Resultado numero par o impar
-  numero = (input("Ingresa un número: ")) 
-  isiteven(numero)
-  #REsultado numero es entero
-  numero1 = input("Ingresa un número: ")
-  isitaninteger(numero1)
