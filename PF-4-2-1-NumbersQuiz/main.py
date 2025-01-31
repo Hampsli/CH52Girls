7061a74f218414fd835e17c0d22013b228ca0b6d
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-01-31 00:58:35 -0600

    Mover los archivos a la carpeta PF-4-2-1-NumberQuiz

---
+++ PF-4-2-1-NumbersQuiz/main.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+def main():
+  random_number = str(5)
+  return random_number
+
+if __name__=="__main__":
+  main()
+
+#take one number as input, and it should output a dictionary of trivia about that number
+def trivia_fetch(number):
+  my_api = "http://numbersapi.com/[number]?json"
+  new_api = my_api.replace("[number]", number)
+  print(new_api)
+  my_request = requests.get(new_api)
+  random_trivia = json.loads(my_request.content)
+  return random_trivia
+
+number = main()
+trivia_fetch(number)
