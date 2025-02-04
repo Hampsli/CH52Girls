6e983987b775e76acb3629ca5e7af4a704b8a83b
AzulSR <139704806+AzulSR@users.noreply.github.com>
2025-02-03 19:31:44 -0600

    Complete el reto de list shoping y hello word en Python y Ruby

---
+++ TIFC1-PF-1-3-20250127T220534Z-001/TIFC1-PF-1-3/PF-1-3-3 ShoppingList-Individual/PF-1-3-3 ShoppingList-Individual/main.py
@@ -0,0 +1,11 @@
+# Solicitar los elementos de la lista
+primer_cosa= input("Ingresa una cosa para una lista de compras: ")
+segunda_cosa= input("Ingresa una segunda cosa para una lista de compras: ")
+tercera_cosa= input("Ingresa una tercera cosa para una lista de compras: ")
+
+# colocar los elementos en la lista
+shopping_list = [primer_cosa, segunda_cosa, tercera_cosa]
+
+# Mostrar la lista final
+print("Lista final:", shopping_list)
+
