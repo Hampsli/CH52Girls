27403ec6f62fa93512758e46162283a121ea7d4b
041152 <47441528+041152@users.noreply.github.com>
2025-02-07 13:56:38 -0600

    Add files via upload

---
+++ sd-l1-1.0/task5.js
@@ -0,0 +1,16 @@
+// Refer to Task 5 in your Instructions to complete this task
+
+for (let i = 0; i < 1; i++) {
+  i = parseInt(console.log("Ingresa un número: ")); 
+  if (i%3 === 0 && i%5 === 0) {
+    console.log("FizzBuzz");}
+  else if (i % 3 === 0) {
+    console.log("Fizz");} 
+  else if (i % 5 === 0){
+    console.log("Buzz");}
+  else if (i % 7 === 0 ){
+    console.log("Woof")
+  }
+  else {
+    console.log(i);}
+  };
