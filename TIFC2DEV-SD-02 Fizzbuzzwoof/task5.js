2ff2a3c537552a63a89f1c8432fc0f3a748ae351
Karla Alonso <karlalonlup@gmail.com>
2025-02-07 20:41:23 -0600

    Add files via upload

---
+++ TIFC2DEV-SD-02 Fizzbuzzwoof/task5.js
@@ -0,0 +1,7 @@
+// Refer to Task 5 in your Instructions to complete this task
+
+for (let i = 0; i < 1; i++) {
+    console.log("This is Task Five!");
+  };
+
+let numLines = parseInt(prompt("Introduce el número de líneas que deseas generar:"));
