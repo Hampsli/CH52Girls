6e3535e4b9d0e4ed97dd27ea5454665e5d1938b9
Ana Elizondo-Salas <77133339+anaelizsa@users.noreply.github.com>
2025-02-08 23:59:46 -0600

    TIFC1-PF-1-3 ShoppingList-Individual/

---
+++ TIFC1-PF-1-3/PF-1-3-3 ShoppingList-Individual/list.py
@@ -0,0 +1,9 @@
+lista_de_compras = [input(), input(), input()]
+
+print("Productos agregados")
+print(lista_de_compras[0])
+print(lista_de_compras[1])
+print(lista_de_compras[2])
+
+print("Lista de productos completa")
+print(lista_de_compras)
