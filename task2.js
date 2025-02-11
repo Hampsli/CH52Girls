c71ad933d2ddb805fd148fe4b4783872b0272126
LizArcs <102216787+LizArcs@users.noreply.github.com>
2025-02-11 00:39:15 -0600

    Lab manipulación de arrays

---
+++ task2.js
@@ -0,0 +1,10 @@
+const arr = [16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+
+// Type your code below this line!
+
+let nuevoarr = [...arr].reverse();
+console.log(nuevoarr);
+
+// Type your code above this line!
+
+arr.forEach(element => console.log(element))
---
+++ task6.js
@@ -0,0 +1,9 @@
+const arr1 = ["hello"]
+const arr2 = ["world"]
+
+// Type your code below this line!
+
+newarr = arr1.concat(arr2);
+console.log(newarr);
+
+// Type your code above this line!
