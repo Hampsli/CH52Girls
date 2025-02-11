5795580f4a0e1b9c55998af8a4c3e29b68d79327
041152 <47441528+041152@users.noreply.github.com>
2025-02-10 20:55:18 -0600

    Add files via upload

---
+++ sd-l2-1.0/task6.js
@@ -0,0 +1,9 @@
+const arr1 = ["hello"]
+const arr2 = ["world"]
+
+// Type your code below this line!
+
+let combinar=arr1.concat(arr2);
+console.log (combinar);
+
+// Type your code above this line!
