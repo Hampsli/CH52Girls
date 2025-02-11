3ad2442ddb77b0abd3d7f9d4299ad1fa12e2517a
Karla Alonso <karlalonlup@gmail.com>
2025-02-10 19:40:17 -0600

    Add files via upload

---
+++ TIFC2-DEV-SD-04 Manipulacion arrays/task1.js
@@ -0,0 +1,9 @@
+const arr = [];
+
+// Type your code below this line!
+
+arr.push(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+
+// Type your code above this line!
+
+arr.forEach(element => console.log(element));
---
+++ TIFC2-DEV-SD-04 Manipulacion arrays/task6.js
@@ -0,0 +1,9 @@
+const arr1 = ["hello"]
+const arr2 = ["world"]
+
+// Type your code below this line!
+
+const primerHola = arr1.concat(arr2);
+console.log(primerHola);
+
+// Type your code above this line!
