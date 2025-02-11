88601f120089e60f55c0087c30ecf1c8d9316e81
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-02-11 09:24:34 -0600

    Agregando la carpeta de ArraysLab

---
+++ SD-04-1 ArraysLab-Individual-1.0/task1.js
@@ -0,0 +1,9 @@
+const arr = []
+
+// Type your code below this line!
+for (let i = 1; i < 21; i++) {
+    arr.push(i);
+}
+// Type your code above this line!
+
+arr.forEach(element => console.log(element))
---
+++ SD-04-1 ArraysLab-Individual-1.0/task4.js
@@ -0,0 +1,7 @@
+const arr = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14]
+
+// Type your code below this line!
+let = duplicateNumber = arr.splice(4, 1)
+// Type your code above this line!
+
+arr.forEach(element => console.log(element))
