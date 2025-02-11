5791b95dc6007b1599b9c82b809ad30ea1f09138
Daniela Rodriguez <dany.rodgarcia@gmail.com>
2025-02-10 22:49:29 -0600

    Agrego laboratorio de arrays

---
+++ sd-l2-1.0/task3.js
@@ -0,0 +1,12 @@
+const arr = [1,2,3,4,5,6,7,9,10,11,12]
+
+// Type your code below this line!
+
+
+   
+    arr.splice(7,0, `8`); //agregar elemento (inicio, cantida, elemento)
+
+
+// Type your code above this line!
+
+arr.forEach(element => console.log(element))
---
+++ sd-l2-1.0/task6.js
@@ -0,0 +1,11 @@
+const arr1 = ["hello"]
+const arr2 = ["world"]
+
+// Type your code below this line!
+
+let arr3 = arr1.concat (arr2);
+console.log (arr3);
+
+
+
+// Type your code above this line!
