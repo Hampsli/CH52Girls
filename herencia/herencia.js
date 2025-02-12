b72b2eadd441a10a05e8d83485c529b05faaab7b
Jenny <jmontanor1800@alumno.ipn.mx>
2025-02-11 21:59:41 -0600

    Herencia, Metodos, Labs SD 06 - SD 08
    Se agregaron los laboratorios y unas carpetas de herencia y métodos con los ejercicios y notas que vimos en los ejercicios.

---
+++ herencia/herencia.js
@@ -0,0 +1,38 @@
+// función/método constructor -> construye objetos movie
+function Movie(name, watched, times) {
+  // this. nos permite acceder a propiedades específicas de mi objeto
+  this.name = String(name);
+  this.watched = Boolean(watched);
+  // en este casi aoellido es un método constructor
+  this.times = Number(times);
+  // en la propiedad info se está asignando un método que no recibe parámetros por lo tanto info es un método
+  this.info = function () {
+    if (this.watched === true) {
+      console.log("I have watched " + this.name + " " + this.times + " times.");
+    } else {
+      console.log("I have not watched " + this.name + " yet.");
+    }
+  };
+}
+
+const watchList = [];
+
+// Vamos a crear un objeto movie por medio del constructor "new Movie"
+const movie1 = new Movie("Son como niños", true, 5);
+const movie2 = new Movie("Bastardos sin gloria", true, 6);
+
+// vamos a incluirla en nuestra lista
+watchList.push(movie1);
+watchList.push(movie2);
+//se manda a llamar o accedemos a su información de la movie1.
+// El método info() se añade a cualquier objeto de película que creemos utilizándolo.
+
+// todos los objetos que se creen apartir de la función movie, tienen el método .info()
+// hereda las características de la función movie
+movie1.info();
+
+// se agrega
+console.log(movie2.name);
+watchList[0].info();
+// imprime toda la lista de películas
+console.log(watchList);
