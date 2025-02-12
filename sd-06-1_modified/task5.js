e8eaa38afeadae5c6b38b5fee0095348f0afe2a3
YesiMachado <machado.yesenia.dev@gmail.com>
2025-02-12 06:56:06 -0800

    Add files via upload

---
+++ sd-06-1_modified/task5.js
@@ -0,0 +1,12 @@
+// Type your code below this line!
+function listaDeAmigos() {
+    this.nombres = [];
+    this.addAmigo = function(nombres){
+        this.amigo.push = prompt(nombres);
+    };
+}
+
+const misAmigos = new listaDeAmigos();
+
+// Type your code above this line!
+
---
+++ sd-06-1_modified/task6.js
@@ -0,0 +1,12 @@
+/*
+* How long should the list be?
+* Not every user will want the same number of items.
+* How should you handle multiples of the same item, such as 2 bottles of milk, or a dozen eggs?
+* How would you collect this data from the user?
+* How would you store this data in an object?
+* What would the constructor function for this object look like?
+*/
+// Type your code below this line!
+
+// Type your code above this line!
+
