d2c81dc3e99780a00c78e52bbbb9afeeb4437ac9
YesiMachado <machado.yesenia.dev@gmail.com>
2025-02-12 07:22:00 -0800

    Add files via upload

---
+++ pf-l4-main calculadora/main.py
@@ -0,0 +1,30 @@
+number1=int(input("ingresa un numero:"))
+number2=int(input("ingresa otro numero:"))
+
+eleccion=0
+while eleccion !=6:
+    print("""
+    indique la operacion a realizar:
+        1)suma
+        2)resta
+        3)multiplicacion  
+        4)division
+        5)salir
+        """)
+    
+    eleccion = int(input())
+
+    if eleccion == 1:
+        print(" ")
+        print("resultado: ",number1, "+", number2, "=", number1+number2)
+    if eleccion == 2:
+        print(" ")
+        print("resultado: ",number1, "-", number2, "=", number1-number2)
+    if eleccion == 3:
+        print(" ")
+        print("resultado: ",number1, "*", number2, "=", number1*number2)
+    if eleccion == 4:
+        print(" ")
+        print("resultado: ",number1, "/", number2, "=", number1/number2)
+    elif eleccion == 5:
+        print("gracias por usar la calculadora. Atte yesi:)")
