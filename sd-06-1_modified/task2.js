74fed12b7caa4edc567e5b815484d78cc734711b
danielarog <158617300+DanielaRoG@users.noreply.github.com>
2025-02-11 22:04:10 -0600

    Tarea Objetos Lab 14.4

---
+++ sd-06-1_modified/task2.js
@@ -0,0 +1,15 @@
+function Mail(subj, msg) {
+    this.subject = subj
+    this.message = msg
+  }
+  
+  // Type your code below this line!
+  
+  
+  
+  const newMail = new Mail (process.argv[3], process.argv[4]);//puede funcionar como un prompt
+  
+  
+  // Type your code above this line!
+  
+  console.log(newMail.subject + ": " + newMail.message)
