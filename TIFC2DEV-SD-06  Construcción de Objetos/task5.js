f1d82808ed607945297bd8f8854969d2ecbae15c
Karla Alonso <karlalonlup@gmail.com>
2025-02-11 23:29:52 -0600

    Add files via upload

---
+++ TIFC2DEV-SD-06  Construcción de Objetos/task5.js
@@ -0,0 +1,20 @@
+
+
+// Type your code below this line!
+function FriendList() {
+    
+    const listName = [];
+    
+    listName.push("Karla");
+    listName.push("Carlota");
+    listName.push("Kay");
+
+    return listName;
+}
+const nombres = FriendList();
+console.log(nombres);  
+
+
+
+// Type your code above this line!
+
