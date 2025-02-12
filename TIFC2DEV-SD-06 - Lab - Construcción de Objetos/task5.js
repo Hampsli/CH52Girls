1a6ebf3dcd54c2685ac7b12903357ea67409c812
M0-00n <m00np1n5678@gmail.com>
2025-02-11 23:44:11 -0600

    Add files via upload

--- TIFC2DEV-SD-06 - Lab - Construcción de Objetos/task5.js
+++ TIFC2DEV-SD-06 - Lab - Construcción de Objetos/task5.js
@@ -6,40 +6,6 @@ Create a constructor for a FriendsList object that will store a list of names in
   * The output should look like this: `[ 'name1', 'name2' ]` */
 
 // Type your code below this line!
-const readline = require("readline");
 
-// Crear la interfaz para recibir entrada del usuario en la terminal
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Constructor FriendsList
-function FriendsList() {
-    this.friends = [];
-}
-
-// Pedir la cantidad de amigos
-rl.question("How many friends do you want to add? ", (num) => {
-    const count = parseInt(num);
-
-    const friendsList = new FriendsList();
-    let i = 0;
-
-    function askForFriend() {
-        if (i < count) {
-            rl.question(`Enter friend ${i + 1}: `, (name) => {
-                friendsList.friends.push(name);
-                i++;
-                askForFriend(); // Pregunta el siguiente nombre
-            });
-        } else {
-            console.log("Your friends list:", friendsList.friends);
-            rl.close(); // Cierra la interfaz después de completar la entrada
-        }
-    }
-
-    askForFriend();
-});
 // Type your code above this line!
 
