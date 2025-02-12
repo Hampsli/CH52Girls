c2b61200dacdc84eb45f59cdfefa5402c2be1d5c
YesiMachado <machado.yesenia.dev@gmail.com>
2025-02-12 07:21:16 -0800

    Add files via upload

---
+++ pf-l1-1.0 MadLib/main.py
@@ -0,0 +1,35 @@
+print("What is your first name?")
+first_name = input()
+print("What is your last name?")
+last_name = input()
+print("Hey " + first_name + " " + last_name + "!")
+
+
+#solicita nombre (first) y apellido(last)
+print("cual e stu primer nombre?")
+first_name =input ("yesi")
+print("cual es tu apellido?")
+last_name = input("machado")
+
+#saludo al usuario
+Print("Welcome" + first_name + last_name)
+
+#solicita datos al usuario para MadLib
+job_name = input("what is your job name?")
+first_adjective = input ("what is the first adjective for the cohort?")
+second_adjective = ("what is the second adjective")
+first_food = input ("what is the first food?")
+second_food = input ("what is the second food?")
+feeling = input ("how do you feel today?")
+
+#imprime el MadLib con las entradas del usuario
+print (f"{first_name}{last_name} started their first generation course today. They are training as a {job_name}. They found their cohort to be very {first_adjective} but their teacher was, at least, {second_adjective} For lunch they have {first_food} and {second_food} while reviewing their notes. They feel {feeling} but are determined to complete the course")
+
+#¿has entendido el codigo que escribiste?
+#si, en este codigo abordamos el ingreso de datos del usuario y completamos un formulario con los datos que brindo el mismo usuario.
+
+#¿como abordaria esta tarea la proxima vez?
+#si no supiera como hacerlo recurriria a material didactico en internet y resolver mis dudas con alguna IA. si no encuentro nada y tengo muchas dudas recurriria a mi instructora.
+
+#¿que habilidades no tecnicas se han requerido para este tipo de tareas?
+#preseverancia, orientacion al detalle, resolucion de problemas.
