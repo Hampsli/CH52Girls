7def9b67df868be6279a32a95f2fad452b3579b4
041152 <47441528+041152@users.noreply.github.com>
2025-02-11 22:21:23 -0600

    Add files via upload

---
+++ SD-08-1 ClassesLab-Individual-1.0/sd-l4-1.0/task4.js
@@ -0,0 +1,13 @@
+export class Player {
+    constructor(name,level) {
+      this.name=name;
+      this.level=level;
+    }
+    info(){
+      console.log(`${this.name} has reached Level ${this.level}`)
+    }
+    levelUp(){
+      this.level++;
+    }
+  
+  }
