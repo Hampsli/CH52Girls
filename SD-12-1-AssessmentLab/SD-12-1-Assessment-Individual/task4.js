535685857bda82a385a6766d4fc7adf25f485f9e
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-02-14 23:36:06 -0600

    Agregando task4

--- SD-12-1-AssessmentLab/SD-12-1-Assessment-Individual/task4.js
+++ SD-12-1-AssessmentLab/SD-12-1-Assessment-Individual/task4.js
@@ -1,2 +1,23 @@
 // Task 4: delUser(number)
 
+export function delUser(number) {
+    const url = `http://localhost:3000/users/${number}`;
+    
+    fetch(url, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+    .then(response => {
+        if(!response.ok) {
+            throw new Error("Error performing DELETE request");
+        }
+        console.log(`Usser ${number} deleted successfully`);
+    })
+    .catch((error) => {
+        console.error("Error deleting user:", error);
+    });
+}
+
+console.log(delUser(1));
