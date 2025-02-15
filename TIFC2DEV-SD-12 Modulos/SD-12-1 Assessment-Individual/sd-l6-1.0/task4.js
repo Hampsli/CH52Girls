4480e062d037cd717cd81db1dbd95df73ac6f47b
Karla Alonso <karlalonlup@gmail.com>
2025-02-14 22:49:25 -0600

    Update task4.js

--- TIFC2DEV-SD-12 Modulos/SD-12-1 Assessment-Individual/sd-l6-1.0/task4.js
+++ TIFC2DEV-SD-12 Modulos/SD-12-1 Assessment-Individual/sd-l6-1.0/task4.js
@@ -3,23 +3,16 @@
 export function delUser(userId) {
     const url = `http://localhost:3000/users/${userId}`; 
 
-    // Hacer la solicitud DELETE al servidor para eliminar al usuario
     fetch(url, {
-        method: 'DELETE', // Método DELETE para eliminar el usuario
+        method: 'DELETE',
     })
     .then(response => {
         if (response.ok) {
-            console.log(`Usuario con ID ${userId} eliminado con éxito.`);
-            
-            listUsers();  // Mostrar la lista actualizada después de la eliminación
+            console.log(`Usuario con ID ${userId} eliminado con éxito.`);    
         } else {
-            console.log(`Error al intentar eliminar el usuario con ID ${userId}.`);
+            console.error(`Error al eliminar el usuario con ID ${userId}.`);
         }
-    })
-    .catch(error => {
-        console.log(`Error en la solicitud de eliminación: ${error}`);
     });
 }
 
-// Llamar al método para eliminar un usuario con ID 6 (por ejemplo)
-delUser(6);
+delUser(1);
