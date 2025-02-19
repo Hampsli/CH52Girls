2bcfc9f9b1709638874e40a4013349a9419d204d
jessips4 <148427939+jessips4@users.noreply.github.com>
2025-02-19 17:03:23 -0600

    Add files via upload

---
+++ AD-02-1/ad-l1-1.0/script.js
@@ -0,0 +1,11 @@
+document.getElementById("red").innerText = "Adiós";
+
+// Cambiar el color 
+document.getElementById("cambiar").style.color = "orange";
+
+// cambio de color por clic
+var marron = document.getElementById("marron");
+marron.onclick = function (){
+    marron.style.color = "brown"
+}
+
