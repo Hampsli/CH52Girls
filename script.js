d302411f8af11d54baa408a426d53d0eab9a76fb
Diana Caballero <decvargas11@gmail.com>
2025-02-19 16:11:48 -0600

    ad-I1-1.0 Interacting whith HTML,CSS,JS

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("red").textContent = "Adiós";
+
+document.querySelector("h1").style.color = "orange";
+
+document.getElementById("clickable").addEventListener("click", function() {
+    this.style.color = "brown";
+});
+
