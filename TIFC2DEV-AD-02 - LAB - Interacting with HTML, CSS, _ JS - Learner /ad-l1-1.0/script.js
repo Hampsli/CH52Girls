7695736e937060368dab65004473692edb503163
Ana Elizondo-Salas <77133339+anaelizsa@users.noreply.github.com>
2025-02-19 19:19:23 -0600

    Add files via upload

---
+++ TIFC2DEV-AD-02 - LAB - Interacting with HTML, CSS, _ JS - Learner /ad-l1-1.0/script.js
@@ -0,0 +1,5 @@
+document.getElementById("GoodBye").innerHTML = "Goodbye World!"
+document.getElementById("GoodBye").style.color = "orange";
+document.getElementById("clickable-header").addEventListener("click", function() {
+    this.style.color = "brown";
+});
