6edc54ef1919d5d68e4ddeccef99f93f535edd82
BrendaSaldiv <brendas.g.saldivar@gmail.com>
2025-02-21 10:52:47 -0600

    Laboratorio en clase AD-04

---
+++ AD-04/ad-l2-1.0/script.js
@@ -0,0 +1,7 @@
+// Make
+document.querySelectorAll(".RICE").forEach(elemento => {
+    elemento.addEventListener("click", function() {
+      
+       document.getElementById("red").style.color = "red";
+    });
+});
