4a006396ed212070d494c7ac69935b81fda7ce1b
Ana Elizondo-Salas <77133339+anaelizsa@users.noreply.github.com>
2025-02-27 15:39:19 -0600

    AD-04-1 Interaction with HTML- Individual-1.0

---
+++ AD-04-1 Interaction with HTML- Individual-1.0/script.js
@@ -0,0 +1,12 @@
+// Make
+function bievenido(){
+    alert('IT IS BURGER TIME')
+}
+
+function clickOn(){
+    console.log("Someone click on BURGER TOWN!")
+}
+
+function clickOn1(){
+    document.getElementById("imagen").style.color="red"
+}
