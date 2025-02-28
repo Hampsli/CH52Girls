09dc26fe9fcbce73694946e015b8d58d88f0b183
andreaescalanterizo <andrea.esc04@gmail.com>
2025-02-28 16:35:25 -0600

    Add files via upload

---
+++ AD-04-1 Interaction with HTML- Individual-1.0/ad-l2-1.0/script.js
@@ -0,0 +1,13 @@
+function daClick (){
+    alert ("¡ES HORA DE UNA HAMBURGUESA!");
+ }
+
+ function daClick (){
+    console.log ("¡Alguien ha hecho click en BURGER TOWN!");
+ }
+
+function changeTitleColor (){
+    document.getElementById ("burger title").style.color = "red";
+}
+
+red ();
