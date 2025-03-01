a3c444fd472d808c2f6f41f81dcebfbc88fe82c3
jessips4 <148427939+jessips4@users.noreply.github.com>
2025-02-28 18:57:35 -0600

    Delete task2.js

--- task2.js
+++
@@ -1,9 +0,0 @@
-export class FriendNames {
-    constructor(name1,name2,name3) {
-      this.name1 = name1;
-      this.name2 = name2;
-      this.name3 = name3;
-    }
-  }
-  let friend = new FriendNames(process.argv[3], process.argv[4], process.argv[5]);
-  console.log(friend);
