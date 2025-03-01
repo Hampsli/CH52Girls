54c9338740bb52117c53c1c857534bdad1ab1d9c
jessips4 <148427939+jessips4@users.noreply.github.com>
2025-02-28 19:41:48 -0600

    Add files via upload

---
+++ AD-04-1 lab/ad-l2-1.0/script.js
@@ -0,0 +1,12 @@
+// Make
+function burger() {
+    console.log("Alguien hizo clic en COME TO BURGER TOWN!");
+  }
+
+function come(){
+alert("Es hora de una hamburguesa");
+}
+
+function cambiar(){
+    document.getElementById("picture").style.color = "red";
+}
