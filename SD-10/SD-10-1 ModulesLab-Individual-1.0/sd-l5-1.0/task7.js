76177eefd88981a99dd13be7f19952774d1bbe87
jessips4 <148427939+jessips4@users.noreply.github.com>
2025-02-28 19:04:15 -0600

    Add files via upload

---
+++ SD-10/SD-10-1 ModulesLab-Individual-1.0/sd-l5-1.0/task7.js
@@ -0,0 +1,12 @@
+export function rubricPerfect() {
+    if (i == 11) {
+        return "Perfecto";
+    } else if (i <= 9) {
+        return "excelente";
+    } else if ( i>=5) {
+        return "pasa";
+    }else {
+        return "falla"
+    }
+
+}
