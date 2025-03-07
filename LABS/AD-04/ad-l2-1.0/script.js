81a4d2c6ff9dbff917fbf3171783169db1e59a71
itzelpadillac <padillaitzel090@gmail.com>
2025-03-07 17:14:32 -0600

    Add files via upload

---
+++ LABS/AD-04/ad-l2-1.0/script.js
@@ -0,0 +1,12 @@
+// Make
+function daClick(){
+    alert("ES HORA DE UNA HAMBURGUESA");
+}
+
+function burgerClick(){
+    console.log("Alguien hizo clic en BURGER TOWN!");
+}
+
+function clickRed(){
+    document.getElementById("red").style.color="red";
+}
