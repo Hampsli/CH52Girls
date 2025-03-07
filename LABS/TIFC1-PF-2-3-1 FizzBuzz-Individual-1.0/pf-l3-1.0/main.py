48406bcc636b3cbf0742a81082f69898bf87b6fe
itzelpadillac <padillaitzel090@gmail.com>
2025-03-07 17:08:36 -0600

    Add files via upload
    fizz buzz individual

---
+++ LABS/TIFC1-PF-2-3-1 FizzBuzz-Individual-1.0/pf-l3-1.0/main.py
@@ -0,0 +1,15 @@
+
+number = 1
+
+while number <=1000:
+
+ if number % 15 == 0:
+  print("Fizzbuzz")
+ elif number % 3 == 0:
+  print("Fizz")
+ elif number % 5 == 0:
+  print("Buzz")
+ else:
+  print(number)
+  
+ number=number+1
