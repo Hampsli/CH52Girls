a51ecf4f848fbba06ef3b3093f8a4dca29e885a4
itzelpadillac <padillaitzel090@gmail.com>
2025-03-07 17:09:13 -0600

    Add files via upload
    calculadora individual

---
+++ LABS/TIFC1-PF-3-1-1 Calculator-Individual-0.1.0/pf-l4-main/main.py
@@ -0,0 +1,19 @@
+#Calculadora Individual
+num_1=int(input("Ingresa el primer numero: "));
+num_2=int(input("Ingresa el segundo numero: "));
+
+#Resta
+resta=num_1-num_2;
+print("El resultado de la resta es:", resta);
+
+#Multiplicacion
+multi=num_1*num_2;
+print("El resultado de la multiplicacion es:", multi);
+
+#Division
+division=num_1/num_2;
+print("El resultado de la division es:", division);
+
+#Modulo
+mod=num_1%num_2;
+print("El resultado de modulo es:", mod);
